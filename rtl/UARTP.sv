e5dbb476557e7666ce83aa4d1fa881ecb255538e
Teimir <49413936+Teimir@users.noreply.github.com>
2024-06-17 00:26:10 +0300

    180Mhz run

--- rtl/UARTP.sv
+++ rtl/UARTP.sv
@@ -1,5 +1,5 @@
 module UARTP(
-	input clk,
+	input clk_inp,
 	input btn,
 	output led,
 	output led2,
@@ -21,14 +21,13 @@ wire empty_fifo_rx; //Если пустой буффер приёма
 wire full_fifo_tx; //Если заполнен буффер отправки
 wire empty_fifo_tx; //Если пустой буффер отправки
 
-
-
 wire [7:0] fifo_data; //Данные между фифо
 wire [7:0]data_rx; //Данные с рх
 
-
-
 reg flag = 0;  
+reg [1:0] sel_clk = 0;
+wire clk0;
+
 //UART wires
 wire [7:0] rx_used;	//сколько очереди занято под RX
 wire [7:0] tx_used;	//сколько очереди занято под TX
@@ -51,42 +50,52 @@ wire [1:0][31:0] uart_all_data_out = {
 		rx_used
 	}
 };
+
+
+//CLK-PLL
+MainCLKPLL CLPLL(
+	.inclk0	(clk_inp),
+	.c0		(clk0)
+);
+
+
+
 //Подключение модуля RХ
 RX   
 #(    
-    .CLK_FREQ  (50000000)
+    .CLK_FREQ  (180000000)
 )rx_inst(
-    .rx_line	(rx),
-    .clk		(clk),
-    .data		(data_rx),
+    .rx_line		(rx),
+    .clk				(clk0),
+    .data			(data_rx),
     .data_valid	(data_rx_valid),
-	.mode(rx_mode)
+	.mode				(rx_mode)
 );
 
 Fifo_UART RX_FIFO(
-	.clock	(clk),
-	.data	(data_rx),
-	.rdreq	(UART_OP[1] & ~UART_OP[0]),
-	.wrreq	(data_rx_valid),
-	.empty	(empty_fifo_rx),
-	.full	(),
-	.q		(data_from_fifo),
-	.usedw	(rx_used)
+	.clock			(clk0),
+	.data				(data_rx),
+	.rdreq			(UART_OP[1] & ~UART_OP[0]),
+	.wrreq			(data_rx_valid),
+	.empty			(empty_fifo_rx),
+	.full				(),
+	.q					(data_from_fifo),
+	.usedw			(rx_used)
 );
 //Подключение модуля ТХ
 TX 
 #(
-    .CLK_FREQ  (50000000)
+    .CLK_FREQ  (180000000)
 )TX_inst(
     .data_in    (data),
     .data_valid (!empty_fifo_tx),
     .data_ready (data_tx_ready),
-    .clk        (clk),
+    .clk        (clk0),
     .tx_line    (tx),
 	.mode		(tx_mode)
 );  
 Fifo_UART TX_FIFO(
-	.clock	(clk),
+	.clock	(clk0),
 	.data	(data_to_mem),
 	.rdreq	(data_tx_ready),
 	.wrreq	(&UART_OP),
@@ -99,7 +108,7 @@ Fifo_UART TX_FIFO(
 wire [31:0] ram_data_out;
 MEMORY RAM(
 	.address	(ram_addres),
-	.clock		(clk),
+	.clock		(clk0),
 	.data		(data_to_mem),
 	.wren		(RAM_WE),
 	.q			(ram_data_out)
@@ -109,7 +118,7 @@ wire [31:0] data_to_mem;
 wire [12:0] ram_addres;
 wire [1:0] UART_OP;
 CORE u0(
-	.clk(flag & clk),
+	.clk(flag & clk0),
 	.data_to_mem(data_to_mem),
 	.ram_addres(ram_addres),
 	.RAM_WE(RAM_WE),
@@ -118,7 +127,7 @@ CORE u0(
 	.UART_OP(UART_OP)
 );
 
-always @(posedge clk) begin
+always @(posedge clk0) begin
 	if (UART_OP == 2'b1) begin
 		{tx_mode, rx_mode} <= data_to_mem[7:0];
 	end
