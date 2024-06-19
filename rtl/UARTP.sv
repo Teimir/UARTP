fdacacd188a694d5cfd882bb6532b73616bc9139
Teimir <49413936+Teimir@users.noreply.github.com>
2024-06-19 21:11:24 +0300

    working

--- rtl/UARTP.sv
+++ rtl/UARTP.sv
@@ -65,13 +65,6 @@ wire [1:0][31:0] uart_all_data_out = {
 };
 
 
-//CLK-PLL
-MainCLKPLL CLPLL(
-	.inclk0	(clk_inp),
-	.c0		(clk0)
-);
-
-
 
 //Подключение модуля RХ
 RX   
@@ -79,14 +72,14 @@ RX
     .CLK_FREQ  (60000000)
 )rx_inst(
     .rx_line		(rx),
-    .clk				(clk0),
+    .clk				(clk),
     .data			(data_rx),
     .data_valid	(data_rx_valid),
 	.mode				(rx_mode)
 );
 
 Fifo_UART RX_FIFO(
-	.clock			(clk0),
+	.clock			(clk),
 	.data				(data_rx),
 	.rdreq			(UART_OP[1] & ~UART_OP[0]),
 	.wrreq			(data_rx_valid),
@@ -103,12 +96,12 @@ TX
     .data_in    (data),
     .data_valid (!empty_fifo_tx),
     .data_ready (data_tx_ready),
-    .clk        (clk0),
+    .clk        (clk),
     .tx_line    (tx),
 	.mode		(tx_mode)
 );  
 Fifo_UART TX_FIFO(
-	.clock	(clk0),
+	.clock	(clk),
 	.data	(data_to_mem),
 	.rdreq	(data_tx_ready),
 	.wrreq	(&UART_OP),
@@ -121,7 +114,7 @@ Fifo_UART TX_FIFO(
 wire [31:0] ram_data_out;
 MEMORY RAM(
 	.address	(ram_addres),
-	.clock		(clk0),
+	.clock		(clk),
 	.data		(data_to_mem),
 	.wren		(RAM_WE),
 	.q			(ram_data_out)
@@ -131,7 +124,7 @@ wire [31:0] data_to_mem;
 wire [12:0] ram_addres;
 wire [1:0] UART_OP;
 CORE u0(
-	.clk(flag & clk0),
+	.clk(flag & clk),
 	.data_to_mem(data_to_mem),
 	.ram_addres(ram_addres),
 	.RAM_WE(RAM_WE),
